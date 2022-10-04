f451068ff3e9ae9e590915c2e59c2a8d81fb1feb
Theus <matheusth.dev@gmail.com>
2022-10-04 07:44:21 -0300

    feat: query params routes

--- content/urls.py
+++ content/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ContentView.as_view())
+    path("", views.ContentView.as_view()),
+    path("<int:content_id>/", views.ContentParamView.as_view()),
+    path("filter/", views.ContentFilterView.as_view()),
 ]
--- content/views.py
+++ content/views.py
@@ -4,6 +4,10 @@ from rest_framework.views import APIView, Response, status
 from .models import Content
 
 
+class ValidationFieldsError(Exception):
+    pass
+
+
 class ContentView(APIView):
     def get(self, request):
         contents = Content.objects.all()
@@ -48,7 +52,7 @@ class ContentView(APIView):
             "module": data["module"],
             "description": data["description"],
             "students": data["students"],
-            "is_active": data["is_active"]
+            "is_active": data["is_active"],
         }
 
         return result
@@ -62,3 +66,102 @@ class ContentView(APIView):
             return Response(model_to_dict(new_content), status.HTTP_201_CREATED)
         else:
             return Response(val_data, status.HTTP_400_BAD_REQUEST)
+
+
+class ContentParamView(APIView):
+    def get(self, request, content_id):
+        try:
+            content = Content.objects.get(pk=content_id)
+        except Content.DoesNotExist:
+            return Response({"Error": "Content not found"}, status.HTTP_404_NOT_FOUND)
+
+        content_dict = model_to_dict(content)
+
+        return Response(content_dict)
+
+    def data_processing(self, data) -> dict:
+        keys = data.keys()
+        result = {}
+
+        if "title" in keys:
+            result["title"] = data["title"]
+
+        if "module" in keys:
+            result["module"] = data["module"]
+
+        if "description" in keys:
+            result["description"] = data["description"]
+
+        if "students" in keys:
+            result["students"] = data["students"]
+
+        if "is_active" in keys:
+            result["is_active"] = data["is_active"]
+
+        return result
+
+    def validation_update_data(self, data) -> dict or bool:
+        keys = data.keys()
+        result = {}
+
+        if "title" in keys:
+            if type(data["title"]) != str:
+                result["title"] = "must be a str"
+
+        if "module" in keys:
+            if type(data["module"]) != str:
+                result["module"] = "must be a str"
+
+        if "description" in keys:
+            if type(data["description"]) != str:
+                result["description"] = "must be a str"
+
+        if "students" in keys:
+            if type(data["students"]) != int:
+                result["students"] = "must be a int"
+
+        if "is_active" in keys:
+            if type(data["is_active"]) != bool:
+                result["is_active"] = "must be a bool"
+
+        return result
+
+    def patch(self, request, content_id):
+        try:
+            strip_unknow_data = self.data_processing(request.data)
+            no_validate_data = self.validation_update_data(strip_unknow_data)
+
+            if no_validate_data:
+                raise ValidationFieldsError(no_validate_data)
+
+            content = Content.objects.get(id=content_id)
+
+            for key, value in strip_unknow_data.items():
+                setattr(content, key, value)
+
+            content.save()
+
+            return Response(model_to_dict(content))
+        except Content.DoesNotExist:
+            return Response({"Error": "Content not found"}, status.HTTP_404_NOT_FOUND)
+
+        except ValidationFieldsError as err:
+            return Response(err, status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, content_id):
+        try:
+            content = Content.objects.get(id=content_id).delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Content.DoesNotExist:
+            return Response({"Error": "Content not found"}, status.HTTP_404_NOT_FOUND)
+
+
+class ContentFilterView(APIView):
+    def get(self, request):
+        title = request.query_params.get("title", None)
+
+        contents = Content.objects.filter(title=title)
+        contents_dict = [model_to_dict(content) for content in contents]
+
+        return Response(contents_dict)
